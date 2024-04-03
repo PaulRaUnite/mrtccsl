e737bd644aaacf5c66a956aae85b1a1ba5e76621
Pavlo Tokariev <eclipsepaulra@gmail.com>
2024-04-03 21:46:59 +0200

    Added syntax of denotational relations, translation of some constraints.

--- lib/analysis/denotational.ml
+++ lib/analysis/denotational.ml
@@ -1,14 +1,18 @@
+open Sexplib0.Sexp_conv
+
 type num_op =
   | Add
   | Sub
   | Mul
   | Div
+[@@deriving sexp]
 
 type ('c, 'n) tag_expr =
   | TagVar of 'c * int
   | Const of 'n
   | Index of int
   | Op of ('c, 'n) tag_expr * num_op * ('c, 'n) tag_expr
+[@@deriving sexp]
 
 type num_rel =
   | Eq
@@ -16,20 +20,38 @@ type num_rel =
   | Less
   | MoreEq
   | LessEq
+[@@deriving sexp]
 
 type ('c, 'n) bool_expr =
   | Or of ('c, 'n) bool_expr list
   | And of ('c, 'n) bool_expr list
   | Linear of ('c, 'n) tag_expr * num_rel * ('c, 'n) tag_expr
+[@@deriving sexp]
+
+module Syntax = struct
+  let ( @ ) c i = TagVar (c, i)
+  let ( < ) x y = Linear (x, Less, y)
+  let ( <= ) x y = Linear (x, LessEq, y)
+  let ( > ) x y = Linear (x, More, y)
+  let ( >= ) x y = Linear (x, MoreEq, y)
+  let ( = ) x y = Linear (x, Eq, y)
+  let ( + ) x y = Op (x, Add, y)
+  let ( - ) x y = Op (x, Sub, y)
+  let ( * ) x y = Op (x, Mul, y)
+  let ( / ) x y = Op (x, Div, y)
+  let ( |> ) expr (l, r) = And [ l <= expr; expr <= r ]
+end
 
 open Rtccsl
 
 let exact_rel c =
+  let open Syntax in
   let i = 0 in
   (*dummy variable*)
   match c with
-  | Precedence (a, b) -> Linear (TagVar (a, i), Less, TagVar (b, i))
-  | Causality (a, b) -> Linear (TagVar (a, i), LessEq, TagVar (b, i))
+  | Precedence (a, b) -> a @ i < b @ i
+  | Causality (a, b) -> a @ i <= b @ i
+  | RTdelay (arg, out, (e1, e2)) -> ((out @ i) - (arg @ i)) |> (Const e1, Const e2)
   | _ -> failwith "unimplemented"
 ;;
 
