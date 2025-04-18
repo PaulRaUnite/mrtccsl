da623fe21cdea0430e2b22c564a579b1b57cc8bd
Pavlo Tokariev <pavlo.tokariev@inria.fr>
2025-04-18 16:37:09 +0200

    Refactoring of rtccsl structures, added example specifications.

--- lib/rtccsl.ml
+++ lib/rtccsl.ml
@@ -2,16 +2,19 @@ open Prelude
 
 type 'a interval = 'a * 'a
 
+type op =
+  | Add
+  | Sub
+  | Mul
+  | Div
+
 (*TODO: good expression type should:
   - differentiate rational, time and integer expressions types
   - clearly indicate when conversion happens?*)
 type ('v, 'n) expr =
   | Var of 'v
   | Const of 'n
-  | BinAdd of ('v, 'n) expr * ('v, 'n) expr
-  | BinSub of ('v, 'n) expr * ('v, 'n) expr
-  | BinMul of ('v, 'n) expr * ('v, 'n) expr
-  | BinDiv of ('v, 'n) expr * ('v, 'n) expr
+  | Bin of ('v, 'n) expr * op * ('v, 'n) expr
 
 type 'v int_param =
   | IntVar of 'v
@@ -125,10 +128,37 @@ type ('c, 'p, 't) constr =
   | LogicalParameter of 'p * ('p, int) expr interval
   (**Encodes parameter [v] being inside of [[e1, e2]].*)
   (*TODO: strange name, why logical?*)
-  | Pool of int * ('c * 'c) list
-  (**Mutex is a special case of Pool where [n=1]*)
+  | Pool of int * ('c * 'c) list (**Mutex is a special case of Pool where [n=1]*)
 
 type ('c, 'p, 't) specification = ('c, 'p, 't) constr list
+
+let name = function
+  | Precedence _ -> "precedence"
+  | Causality _ -> "causality"
+  | Exclusion _ -> "exclusion"
+  | Coincidence _ -> "coincidence"
+  | Subclocking _ -> "subclocking"
+  | Minus _ -> "minus"
+  | Delay _ -> "delay"
+  | Fastest _ -> "fastest"
+  | Slowest _ -> "slowest"
+  | Intersection _ -> "intersection"
+  | Union _ -> "union"
+  | Periodic _ -> "periodic"
+  | Sample _ -> "sample"
+  | Alternate _ -> "alternate"
+  | RTdelay _ -> "rtdelay"
+  | CumulPeriodic _ -> "cumulperiodic"
+  | AbsPeriodic _ -> "absperiodic"
+  | FirstSampled _ -> "first_sampled"
+  | LastSampled _ -> "last_sampled"
+  | Forbid _ -> "forbid"
+  | Allow _ -> "allow"
+  | Sporadic _ -> "sporadic"
+  | TimeParameter (_, _) -> "time_param"
+  | LogicalParameter (_, _) -> "int_param"
+  | Pool (_, _) -> "pool"
+;;
 
 let clocks = function
   | Precedence { cause; conseq } | Causality { cause; conseq } -> [ cause; conseq ]
@@ -153,3 +183,180 @@ let clocks = function
 ;;
 
 let spec_clocks l = List.flat_map clocks l
+
+let int_param_map f = function
+  | IntConst c -> IntConst c
+  | IntVar v -> IntVar (f v)
+;;
+
+let time_param_map_const f = function
+  | TimeVar v -> TimeVar v
+  | TimeConst c -> TimeConst (f c)
+;;
+
+let rec expr_map fv fc = function
+  | Var v -> Var (fv v)
+  | Const c -> Const (fc c)
+  | Bin (l, op, r) -> Bin (expr_map fv fc l, op, expr_map fv fc r)
+;;
+
+let map
+      (type c nc p np t nt)
+      (fc : c -> nc)
+      (fp : p -> np)
+      (fparam : (p, t) time_param -> (np, nt) time_param)
+      (fexp : (p, t) expr -> (np, nt) expr)
+      (c : (c, p, t) constr)
+  : (nc, np, nt) constr
+  =
+  match c with
+  | Precedence { cause; conseq } -> Precedence { cause = fc cause; conseq = fc conseq }
+  | Causality { cause; conseq } -> Causality { cause = fc cause; conseq = fc conseq }
+  | Exclusion list -> Exclusion (List.map fc list)
+  | Coincidence list -> Coincidence (List.map fc list)
+  | Subclocking { sub; super } -> Subclocking { sub = fc sub; super = fc super }
+  | Minus { out; arg; except } ->
+    Minus { out = fc out; arg = fc arg; except = List.map fc except }
+  | Delay { out; arg; delay; base } ->
+    Delay
+      { out = fc out
+      ; arg = fc arg
+      ; delay = Tuple.map2 (int_param_map fp) delay
+      ; base = Option.map fc base
+      }
+  | Fastest { out; left; right } ->
+    Fastest { out = fc out; left = fc left; right = fc right }
+  | Slowest { out; left; right } ->
+    Slowest { out = fc out; left = fc left; right = fc right }
+  | Intersection { out; args } -> Intersection { out = fc out; args = List.map fc args }
+  | Union { out; args } -> Union { out = fc out; args = List.map fc args }
+  | Periodic { out; base; period } ->
+    Periodic { out = fc out; base = fc base; period = int_param_map fp period }
+  | Sample { out; arg; base } -> Sample { out = fc out; arg = fc arg; base = fc base }
+  | Alternate { first; second } -> Alternate { first = fc first; second = fc second }
+  | RTdelay { out; arg; delay } ->
+    let l, r = delay in
+    RTdelay { out = fc out; arg = fc arg; delay = fparam l, fparam r }
+  | CumulPeriodic { out; period; error; offset } ->
+    let l, r = error in
+    CumulPeriodic
+      { out = fc out
+      ; period = fparam period
+      ; error = fparam l, fparam r
+      ; offset = fparam offset
+      }
+  | AbsPeriodic { out; period; error; offset } ->
+    let l, r = error in
+    AbsPeriodic
+      { out = fc out
+      ; period = fparam period
+      ; error = fparam l, fparam r
+      ; offset = fparam offset
+      }
+  | FirstSampled { out; arg; base } ->
+    FirstSampled { out = fc out; arg = fc arg; base = fc base }
+  | LastSampled { out; arg; base } ->
+    LastSampled { out = fc out; arg = fc arg; base = fc base }
+  | Forbid { from; until; args } ->
+    Forbid { from = fc from; until = fc until; args = List.map fc args }
+  | Allow { from; until; args } ->
+    Allow { from = fc from; until = fc until; args = List.map fc args }
+  | Sporadic { out; at_least; strict } ->
+    Sporadic { out = fc out; at_least = fparam at_least; strict }
+  | TimeParameter (v, (l, r)) -> TimeParameter (fp v, (fexp l, fexp r))
+  | LogicalParameter (v, (l, r)) ->
+    LogicalParameter (fp v, (expr_map fp Fun.id l, expr_map fp Fun.id r))
+  | Pool (n, pairs) -> Pool (n, List.map (Tuple.map2 fc) pairs)
+;;
+
+let map_time_const f = map Fun.id Fun.id (time_param_map_const f) (expr_map Fun.id f)
+
+module Macro = struct
+  let task_clocks name =
+    let start = Printf.sprintf "%s.s" name in
+    let finish = Printf.sprintf "%s.f" name in
+    let ready = Printf.sprintf "%s.r" name in
+    let deadline = Printf.sprintf "%s.d" name in
+    name, ready, start, finish, deadline
+  ;;
+
+  let task name exec_duration =
+    let _, ready, start, finish, _ = task_clocks name in
+    [ Causality { cause = ready; conseq = start }
+    ; RTdelay
+        { out = finish
+        ; arg = start
+        ; delay = Tuple.map2 (fun x -> TimeConst x) exec_duration
+        }
+    ]
+  ;;
+
+  let task_with_deadline name exec_duration deadline =
+    let finish = Printf.sprintf "%s.f" name in
+    Precedence { cause = finish; conseq = deadline } :: task name exec_duration
+  ;;
+
+  let periodic_task name exec_duration period (nerror, perror) offset =
+    let ready = Printf.sprintf "%s.r" name in
+    let timer = Printf.sprintf "%s.t" name in
+    let deadline = Printf.sprintf "%s.d" name in
+    task_with_deadline name exec_duration deadline
+    @ [ CumulPeriodic
+          { out = timer
+          ; period = TimeConst period
+          ; error = TimeConst nerror, TimeConst perror
+          ; offset = TimeConst offset
+          }
+      ; Delay
+          { out = deadline
+          ; arg = timer
+          ; delay = IntConst 1, IntConst 1
+          ; base = Some timer
+          }
+      ; Coincidence [ timer; ready ]
+      ]
+  ;;
+
+  let scheduling_pairs tasks =
+    List.map
+      (fun name ->
+         let _, _, start, finish, _ = task_clocks name in
+         start, finish)
+      tasks
+  ;;
+end
+
+module Examples = struct
+  open Macro
+
+  module SimpleControl = struct
+    let no_resource_constraint =
+      List.flatten
+        [ periodic_task "s" (10, 15) 50 (-2, 2) 0
+        ; periodic_task "a" (5, 10) 50 (-2, 2) 0
+        ; task "c" (25, 40)
+        ; [ Coincidence [ "s.f"; "c.r" ]; Alternate { first = "c.s"; second = "c.f" } ]
+        ]
+    ;;
+
+    let par_pipeline =
+      List.flatten
+        [ [ Alternate { first = "a.s"; second = "a.f" }
+          ; Alternate { first = "s.s"; second = "s.f" }
+          ; Alternate { first = "c.s"; second = "c.f" }
+          ]
+        ; no_resource_constraint
+        ]
+    ;;
+
+    let shared_2core =
+      Pool (2, scheduling_pairs [ "a"; "s"; "c" ]) :: no_resource_constraint
+    ;;
+
+    let sa_group =
+      Pool (1, scheduling_pairs [ "a"; "s" ])
+      :: Pool (1, scheduling_pairs [ "c" ])
+      :: no_resource_constraint
+    ;;
+  end
+end
