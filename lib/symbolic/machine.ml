7f0d2e4bafb1b257a44a6f2409171d32ae153107
Pavlo Tokariev <pavlo.tokariev@inria.fr>
2026-02-02 17:48:22 +0100

    feat(symbolic): implemented machine synchronization and transition driving

--- lib/symbolic/machine.ml
+++ lib/symbolic/machine.ml
@@ -21,11 +21,14 @@ module Queue = struct
   let for_all = List.for_all
 end
 
+open Ppx_compare_lib.Builtin
+
 type ('b, 'v) ite =
   { cond : 'b
   ; if_true : 'v
   ; if_false : 'v
   }
+[@@deriving compare]
 
 (** Empty type. It has no constructors and so values cannot exist.
   Used to prune possibilities in polymorphic types.*)
@@ -78,6 +81,7 @@ and ('sv, 'iv) rat_queue_expr =
   | RPushQueue of ('sv, 'iv) rat_queue_expr * ('sv, 'iv) rat_expr
   | RPopQueue of ('sv, 'iv) rat_queue_expr
   | RQITE of (('sv, 'iv) bool_expr, ('sv, 'iv) rat_queue_expr) ite
+[@@deriving compare]
 
 type ('sv, 'iv) expr =
   | BoolExpr of ('sv, 'iv) bool_expr
@@ -85,10 +89,12 @@ type ('sv, 'iv) expr =
   | RatExpr of ('sv, 'iv) rat_expr
   | IntQueueExpr of ('sv, 'iv) int_queue_expr
   | RatQueueExpr of ('sv, 'iv) rat_queue_expr
+[@@deriving compare]
 
-type ('sv, 'iv) guard = ('sv, 'iv) bool_expr
-type ('sv, 'iv) assignment = 'sv * ('sv, 'iv) expr
+type ('sv, 'iv) guard = ('sv, 'iv) bool_expr [@@deriving compare]
+type ('sv, 'iv) assignment = 'sv * ('sv, 'iv) expr [@@deriving compare]
 
+(** Transition type. Consists of conditions on state (preselect), inputs and state, and state update. *)
 type ('sv, 'iv) transition =
   { state_cond : ('sv, empty) bool_expr
     (** Condition on state variables that identifies the partition. *)
@@ -98,159 +104,297 @@ type ('sv, 'iv) transition =
     (** List of actions performed on the state variables using previous state and inputs [S * B^n * Q -> S]*)
   }
 
+(** Synchronizes two transitions into a new transition. A conjunction of conditions, union of assignments. *)
+let sync_transitions
+      { state_cond = s1; input_cond = in1; assignments = asgn1 }
+      { state_cond = s2; input_cond = in2; assignments = asgn2 }
+  =
+  let assignments = List.sort_uniq compare (List.append asgn1 asgn2) in
+  assert (
+    (* checking that there are no duplicate, non equivalent assignments *)
+    snd
+    @@ List.fold_left
+         (fun ((prev, duplicated) as acc) el ->
+            if duplicated
+            then acc
+            else (
+              match prev with
+              (*FIXME: I use polymorphic equality here, should be fine, but better to replace it when functor is introduced. *)
+              | Some prev -> if prev = el then Some el, true else Some el, false
+              | None -> Some el, false))
+         (None, true)
+         assignments);
+  { state_cond = BAnd [ s1; s2 ]; input_cond = BAnd [ in1; in2 ]; assignments }
+;;
+
+(** Type of abstract machines. *)
 type ('sv, 'iv) t =
   { transitions : ('sv, 'iv) transition list
   ; invariant : ('sv, empty) bool_expr
   }
 
-type inputs =
-  { rationals : Rational.t VarMap.t
-  ; integers : int VarMap.t
-  ; bools : bool VarMap.t
-  }
-
-type state =
-  { integers : int VarMap.t (* 0 *)
-  ; rationals : Rational.t VarMap.t (* -1 *)
-  ; bools : bool VarMap.t (* false *)
-  ; int_queues : int Queue.t VarMap.t (* [] *)
-  ; rat_queues : Rational.t Queue.t VarMap.t (* [] *)
-  }
-
-let rec eval_bool state inputs =
-  let evalb = eval_bool state inputs in
-  function
-  | BConst c -> c
-  | BStateVar v -> VarMap.value ~default:false v state.bools
-  | BInputVar v -> VarMap.value ~default:false v inputs.bools
-  | BNot e -> not (evalb e)
-  | BAnd conjunctions -> List.for_all evalb conjunctions
-  | BOr disjunctions -> List.exists evalb disjunctions
-  | BEq (e1, e2) -> Bool.equal (evalb e1) (evalb e2)
-  | BNeq (e1, e2) -> not (Bool.equal (eval_bool state inputs e1) (evalb e2))
-  | BImply (e1, e2) -> (not (evalb e1)) || evalb e2
-  | IntComp (e1, rel, e2) ->
-    do_rel Int.compare rel (eval_integer state inputs e1) (eval_integer state inputs e2)
-  | RatComp (e1, rel, e2) ->
-    do_rel
-      Rational.compare
-      rel
-      (eval_rational state inputs e1)
-      (eval_rational state inputs e2)
-  | BITE { cond; if_true; if_false } ->
-    if evalb cond then evalb if_true else evalb if_false
-  | IntQueuePositive q ->
-    Queue.for_all (Integer.less_eq 0) (eval_int_queue state inputs q)
-
-and eval_integer state inputs = function
-  | IConst c -> c
-  | IStateVar var -> VarMap.value ~default:0 var state.integers
-  | IInputVar var -> VarMap.value ~default:0 var inputs.integers
-  | IBinOp (e1, op, e2) ->
-    let e1 = eval_integer state inputs e1
-    and e2 = eval_integer state inputs e2 in
-    (match op with
-     | `Div -> Int.div e1 e2
-     | `Add -> Int.add e1 e2
-     | `Sub -> Int.sub e1 e2
-     | `Mul -> Int.mul e1 e2)
-  | IPeekFirstQueue qe ->
-    let qe = eval_int_queue state inputs qe in
-    Queue.peek qe
-  | IPeekLastQueue qe ->
-    let qe = eval_int_queue state inputs qe in
-    Queue.last qe
-  | IntQueueLength qe ->
-    let qe = eval_int_queue state inputs qe in
-    Queue.length qe
-  | RatQueueLength qe ->
-    let qe = eval_rat_queue state inputs qe in
-    Queue.length qe
-  | IITE { cond; if_true; if_false } ->
-    if eval_bool state inputs cond
-    then eval_integer state inputs if_true
-    else eval_integer state inputs if_false
-
-and eval_rational state inputs = function
-  | RConst c -> c
-  | RStateVar var -> VarMap.value ~default:Rational.zero var state.rationals
-  | RInputVar var -> VarMap.value ~default:Rational.zero var inputs.rationals
-  | RBinOp (e1, op, e2) ->
-    let e1 = eval_rational state inputs e1
-    and e2 = eval_rational state inputs e2 in
-    (match op with
-     | `Div -> Rational.div e1 e2
-     | `Add -> Rational.add e1 e2
-     | `Sub -> Rational.sub e1 e2
-     | `Mul -> Rational.mul e1 e2)
-  | RPeekFirstQueue qe ->
-    let qe = eval_rat_queue state inputs qe in
-    Queue.peek qe
-  | RPeekLastQueue qe ->
-    let qe = eval_rat_queue state inputs qe in
-    Queue.last qe
-  | RITE { cond; if_true; if_false } ->
-    if eval_bool state inputs cond
-    then eval_rational state inputs if_true
-    else eval_rational state inputs if_false
-
-and eval_int_queue state inputs = function
-  | IQVar var -> VarMap.value ~default:Queue.empty var state.int_queues
-  | IPushQueue (q, e) ->
-    let e = eval_integer state inputs e
-    and q = eval_int_queue state inputs q in
-    Queue.push e q
-  | IPopQueue q ->
-    let q = eval_int_queue state inputs q in
-    Queue.pop q
-  | IDecreaseAllQueue q -> Queue.map Int.pred (eval_int_queue state inputs q)
-  | IQITE { cond; if_true; if_false } ->
-    eval_int_queue
-      state
-      inputs
-      (if eval_bool state inputs cond then if_true else if_false)
-
-and eval_rat_queue state inputs = function
-  | RQVar var -> VarMap.value ~default:Queue.empty var state.rat_queues
-  | RPushQueue (q, e) ->
-    let e = eval_rational state inputs e
-    and q = eval_rat_queue state inputs q in
-    Queue.push e q
-  | RPopQueue q ->
-    let q = eval_rat_queue state inputs q in
-    Queue.pop q
-  | RQITE { cond; if_true; if_false } ->
-    eval_rat_queue
-      state
-      inputs
-      (if eval_bool state inputs cond then if_true else if_false)
+(** Synchronizes two machines by cartesian product of their transitions and conjunction of invariants. *)
+let sync_machines
+      { transitions = t1; invariant = in1 }
+      { transitions = t2; invariant = in2 }
+  =
+  let transitions = List.cartesian t1 t2 |> List.map (Tuple.fn2 sync_transitions) in
+  { transitions; invariant = BAnd [ in1; in2 ] }
 ;;
 
-let transition state inputs assignment =
-  let var, expr = assignment in
-  let state =
+(** Module of value based interpretation of the abstract machine. *)
+module Interpretation = struct
+  type var = string
+
+  (** Type of input values. *)
+  type inputs =
+    { rationals : Rational.t VarMap.t
+    ; integers : int VarMap.t
+    ; bools : bool VarMap.t
+    }
+
+  (* Default (unassigned/empty) inputs. *)
+  let default_inputs =
+    { rationals = VarMap.empty; integers = VarMap.empty; bools = VarMap.empty }
+  ;;
+
+  (** Type of state values. *)
+  type state =
+    { integers : int VarMap.t (* 0 *)
+    ; rationals : Rational.t VarMap.t (* -1 *)
+    ; bools : bool VarMap.t (* false *)
+    ; int_queues : int Queue.t VarMap.t (* [] *)
+    ; rat_queues : Rational.t Queue.t VarMap.t (* [] *)
+    }
+
+  (** Default (unassigned/empty) state. *)
+  let default_state =
+    { rationals = VarMap.empty
+    ; integers = VarMap.empty
+    ; bools = VarMap.empty
+    ; int_queues = VarMap.empty
+    ; rat_queues = VarMap.empty
+    }
+  ;;
+
+  type ('v, 'a) value_getter = default:'a -> 'v -> 'a
+
+  type 'v state_interface =
+    { integer : ('v, int) value_getter
+    ; rational : ('v, Rational.t) value_getter
+    ; bool : ('v, bool) value_getter
+    ; int_queue : ('v, int Queue.t) value_getter
+    ; rat_queue : ('v, Rational.t Queue.t) value_getter
+    }
+
+  let state_to_interface { rationals; integers; bools; int_queues; rat_queues } =
+    { integer = (fun ~default v -> VarMap.value ~default v integers)
+    ; rational = (fun ~default v -> VarMap.value ~default v rationals)
+    ; bool = (fun ~default v -> VarMap.value ~default v bools)
+    ; int_queue = (fun ~default v -> VarMap.value ~default v int_queues)
+    ; rat_queue = (fun ~default v -> VarMap.value ~default v rat_queues)
+    }
+  ;;
+
+  type 'v input_interface =
+    { integer : ('v, int) value_getter
+    ; rational : ('v, Rational.t) value_getter
+    ; bool : ('v, bool) value_getter
+    }
+
+  let empty_input_interface : empty input_interface =
+    let impossible ~default:_ _ =
+      failwith "impossible situation, empty type has inhabitants"
+    in
+    { integer = impossible; rational = impossible; bool = impossible }
+  ;;
+
+  let input_to_interface ({ rationals; integers; bools } : inputs) =
+    { integer = (fun ~default v -> VarMap.value ~default v integers)
+    ; rational = (fun ~default v -> VarMap.value ~default v rationals)
+    ; bool = (fun ~default v -> VarMap.value ~default v bools)
+    }
+  ;;
+
+  (** Evaluate Boolean formula given state and input values. *)
+  let rec eval_bool (state : 'sv state_interface) (inputs : 'iv input_interface)
+    : ('sv, 'iv) bool_expr -> bool
+    =
+    let evalb = eval_bool state inputs in
+    function
+    | BConst c -> c
+    | BStateVar v -> state.bool ~default:false v
+    | BInputVar v -> inputs.bool ~default:false v
+    | BNot e -> not (evalb e)
+    | BAnd conjunctions -> List.for_all evalb conjunctions
+    | BOr disjunctions -> List.exists evalb disjunctions
+    | BEq (e1, e2) -> Bool.equal (evalb e1) (evalb e2)
+    | BNeq (e1, e2) -> not (Bool.equal (eval_bool state inputs e1) (evalb e2))
+    | BImply (e1, e2) -> (not (evalb e1)) || evalb e2
+    | IntComp (e1, rel, e2) ->
+      do_rel Int.compare rel (eval_integer state inputs e1) (eval_integer state inputs e2)
+    | RatComp (e1, rel, e2) ->
+      do_rel
+        Rational.compare
+        rel
+        (eval_rational state inputs e1)
+        (eval_rational state inputs e2)
+    | BITE { cond; if_true; if_false } ->
+      if evalb cond then evalb if_true else evalb if_false
+    | IntQueuePositive q ->
+      Queue.for_all (Integer.less_eq 0) (eval_int_queue state inputs q)
+
+  (** Evaluate integer expression given state and input values. *)
+  and eval_integer state inputs = function
+    | IConst c -> c
+    | IStateVar var -> state.integer ~default:0 var
+    | IInputVar var -> inputs.integer ~default:0 var
+    | IBinOp (e1, op, e2) ->
+      let e1 = eval_integer state inputs e1
+      and e2 = eval_integer state inputs e2 in
+      (match op with
+       | `Div -> Int.div e1 e2
+       | `Add -> Int.add e1 e2
+       | `Sub -> Int.sub e1 e2
+       | `Mul -> Int.mul e1 e2)
+    | IPeekFirstQueue qe ->
+      let qe = eval_int_queue state inputs qe in
+      Queue.peek qe
+    | IPeekLastQueue qe ->
+      let qe = eval_int_queue state inputs qe in
+      Queue.last qe
+    | IntQueueLength qe ->
+      let qe = eval_int_queue state inputs qe in
+      Queue.length qe
+    | RatQueueLength qe ->
+      let qe = eval_rat_queue state inputs qe in
+      Queue.length qe
+    | IITE { cond; if_true; if_false } ->
+      if eval_bool state inputs cond
+      then eval_integer state inputs if_true
+      else eval_integer state inputs if_false
+
+  (** Evaluate rational expression given state and input values. *)
+  and eval_rational state inputs = function
+    | RConst c -> c
+    | RStateVar var -> state.rational ~default:Rational.minusone var
+    | RInputVar var -> inputs.rational ~default:Rational.minusone var
+    | RBinOp (e1, op, e2) ->
+      let e1 = eval_rational state inputs e1
+      and e2 = eval_rational state inputs e2 in
+      (match op with
+       | `Div -> Rational.div e1 e2
+       | `Add -> Rational.add e1 e2
+       | `Sub -> Rational.sub e1 e2
+       | `Mul -> Rational.mul e1 e2)
+    | RPeekFirstQueue qe ->
+      let qe = eval_rat_queue state inputs qe in
+      Queue.peek qe
+    | RPeekLastQueue qe ->
+      let qe = eval_rat_queue state inputs qe in
+      Queue.last qe
+    | RITE { cond; if_true; if_false } ->
+      if eval_bool state inputs cond
+      then eval_rational state inputs if_true
+      else eval_rational state inputs if_false
+
+  (** Evaluate integer queue expression given state and input values. *)
+  and eval_int_queue state inputs = function
+    | IQVar var -> state.int_queue ~default:Queue.empty var
+    | IPushQueue (q, e) ->
+      let e = eval_integer state inputs e
+      and q = eval_int_queue state inputs q in
+      Queue.push e q
+    | IPopQueue q ->
+      let q = eval_int_queue state inputs q in
+      Queue.pop q
+    | IDecreaseAllQueue q -> Queue.map Int.pred (eval_int_queue state inputs q)
+    | IQITE { cond; if_true; if_false } ->
+      eval_int_queue
+        state
+        inputs
+        (if eval_bool state inputs cond then if_true else if_false)
+
+  (** Evaluate rational queue expression given state and input values. *)
+  and eval_rat_queue state inputs = function
+    | RQVar var -> state.rat_queue ~default:Queue.empty var
+    | RPushQueue (q, e) ->
+      let e = eval_rational state inputs e
+      and q = eval_rat_queue state inputs q in
+      Queue.push e q
+    | RPopQueue q ->
+      let q = eval_rat_queue state inputs q in
+      Queue.pop q
+    | RQITE { cond; if_true; if_false } ->
+      eval_rat_queue
+        state
+        inputs
+        (if eval_bool state inputs cond then if_true else if_false)
+  ;;
+
+  (** Applies the assignment, evaluating on the given state and input values, and saves the value in new state. *)
+  let apply_assignment old_state inputs new_state assignment =
+    let var, expr = assignment in
     match expr with
     | BoolExpr e ->
-      { state with bools = VarMap.add var (eval_bool state inputs e) state.bools }
+      { new_state with
+        bools = VarMap.add var (eval_bool old_state inputs e) new_state.bools
+      }
     | IntExpr e ->
-      { state with
-        integers = VarMap.add var (eval_integer state inputs e) state.integers
+      { new_state with
+        integers = VarMap.add var (eval_integer old_state inputs e) new_state.integers
       }
     | RatExpr e ->
-      { state with
-        rationals = VarMap.add var (eval_rational state inputs e) state.rationals
+      { new_state with
+        rationals = VarMap.add var (eval_rational old_state inputs e) new_state.rationals
       }
     | IntQueueExpr e ->
-      { state with
-        int_queues = VarMap.add var (eval_int_queue state inputs e) state.int_queues
+      { new_state with
+        int_queues =
+          VarMap.add var (eval_int_queue old_state inputs e) new_state.int_queues
       }
     | RatQueueExpr e ->
-      { state with
-        rat_queues = VarMap.add var (eval_rat_queue state inputs e) state.rat_queues
+      { new_state with
+        rat_queues =
+          VarMap.add var (eval_rat_queue old_state inputs e) new_state.rat_queues
       }
-  in
-  state
-;;
+  ;;
+
+  (** Applies all assignments, evaluating on the given state and input values, and saves values in new state. *)
+  let apply_assignments old_state inputs new_state assignments =
+    List.fold_left
+      (fun new_state assgn -> apply_assignment old_state inputs new_state assgn)
+      new_state
+      assignments
+  ;;
+
+  type transition_error =
+    | NoValidTransition
+    | FailedGuard
+    | FailedInvariant
+
+  let apply_transition { transitions; invariant } state inputs =
+    let state = state_to_interface state in
+    let inputs = input_to_interface inputs in
+    let empty_inputs = empty_input_interface in
+    let transition =
+      List.find_opt
+        (fun { state_cond; _ } -> eval_bool state empty_inputs state_cond)
+        transitions
+    in
+    match transition with
+    | Some { input_cond; assignments; _ } ->
+      if eval_bool state inputs input_cond
+      then (
+        let new_state = apply_assignments state inputs default_state assignments in
+        let new_state_int = state_to_interface new_state in
+        if eval_bool new_state_int empty_inputs invariant
+        then Ok new_state
+        else Error FailedInvariant)
+      else Error FailedGuard
+    | None -> Error NoValidTransition
+  ;;
+end
 
 module Syntax = struct
   (** Syntax for the machine creation. *)
